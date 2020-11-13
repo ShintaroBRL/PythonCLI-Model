19aebf7ac565601a11161fcd925535a32a9a0776
ShintaroBRL <juliano0forum@gmail.com>
2020-11-13 11:15:36 -0300

    Initial Commit

--- start.py
+++ start.py
@@ -1,14 +1,58 @@
 #installed modules
 from pyfiglet import Figlet
-from clint.textui import colored, puts
+from clint.textui import colored
+from progressbar import *
 import argparse
+import signal
 
 #system modules
+import time
 import sys
 import os
 
-#Configs
-os.system('cls' if os.name == 'nt' else 'clear')
-sys.path.insert(0, os.path.abspath('..'))
+#Metas
+__author__ = "Juliano Lira Florentino"
+__description__ = "Programa desenvolvido para tals"
 
-print colored.yellow(Figlet(font='slant').renderText('Login Flood'))
+#Configs
+os.system('cls' if os.name == 'nt' else 'clear')    #Clear screen on start
+parser = argparse.ArgumentParser()                  # Initialize parser
+ 
+#Args configs
+parser.add_argument("--output", help = "Show Output")
+parser.add_argument("--input", help = "Show input", type=int, required=True)
+args = parser.parse_args()
+
+#Print welcome
+print colored.yellow(Figlet(font='slant').renderText('Model'))
+print colored.yellow("author: "+__author__)
+print colored.yellow("Description: "+__description__+"\n")
+
+#Functions
+def onFinish():
+    print colored.yellow("Processamento finalizado")
+
+def onStart():
+    print colored.yellow("Processamento iniciado")
+
+def onRun(index):
+    time.sleep(0.5)
+    print "Processamento Run"+str(index)
+
+def Progress(max,callback,callstart,callrun):
+    callstart()
+    widgets=[
+        RotatingMarker(),
+        ' [', Timer(), '] ',
+        Bar(),
+        ' (', ETA(),')',
+    ]
+    for i in progressbar(range(max), widgets=widgets, redirect_stdout=True):
+        callrun(i)
+    callback()
+
+def keyboardInterruptHandler(signal, frame):
+    exit(0)
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+Progress(100,onFinish,onStart,onRun)
